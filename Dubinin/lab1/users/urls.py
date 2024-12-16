ca553d7facdd52b459c2a185ec2901d442921a50
ST3505 <ST3505@TE.local>
2024-12-16 16:11:52 +0600

    FINISH_LAB3

--- Dubinin/lab1/users/urls.py
+++ Dubinin/lab1/users/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = 'register'
+app_name = 'users'
 
 urlpatterns = [
-  path('', views.register_view, name="register"),
+  path('register/', views.register_view, name="register"),
+  path('login/', views.login_view, name="login"),
 ]
